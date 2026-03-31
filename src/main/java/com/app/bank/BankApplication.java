8dd83029a5318efffe8ca26e8fa88decc8ae17d3
Franck ludovic <franckludovic351@gmail.com>
2026-03-31 16:45:31 +0100

    updated version with swagger-ui documentation #Backend#

--- src/main/java/com/app/bank/BankApplication.java
+++ src/main/java/com/app/bank/BankApplication.java
@@ -1,9 +1,35 @@
 package com.app.bank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Secure Bank API",
+                version = "3.0",
+                description = "A secure and robust Bank Transaction API built with Spring Boot, Java, and MySQL.\n" +
+                        "This project demonstrates best practices for building a backend banking system including account management and transaction processing.",
+                contact = @Contact(
+                        name = "Franck Ludovic",
+                        email = "franckludovic351@gmail.com",
+                        url = "https://github.com/ludovicfranck/Transaction-Bank-app-"
+                ),
+                license = @License(
+                        name = "Secure Bank API",
+                        url = "https://github.com/ludovicfranck/Transaction-Bank-app-"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Documentation of a secure bank API",
+                url = "https://github.com/ludovicfranck/Transaction-Bank-app-"
+        )
+)
 public class BankApplication {
 
 	public static void main(String[] args) {
